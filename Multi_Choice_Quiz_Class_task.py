35586ebcec51f0d5836e37e85eaa874ca4c680ec
Tong Chan <86693694+Xadeo@users.noreply.github.com>
2022-04-01 10:20:18 +1300

    Added prompt Label and question Label

--- Multi_Choice_Quiz_Class_task.py
+++ Multi_Choice_Quiz_Class_task.py
@@ -4,6 +4,18 @@ from tkinter import *
 
 class Multi_ChoiceGUI:
     def __init__(self, parent):
+        self.choice_var = StringVar()
+        self.choice_var.set("")
+        
+        self.font = ("Comic Sans MS", "10", "bold")
+
+        prompt_Label = Label(parent, text = "Question.")
+        prompt_Label.grid(row = 0, column = 0)
+
+        quesiton_Label = Label(parent, text = "What is the capital of Mongolia?", font = self.font,  width = 40, height = 30)
+        quesiton_Label.grid(row = 0, column = 1)
+
+
 
 #main routine
 if __name__ == "__main__":        
