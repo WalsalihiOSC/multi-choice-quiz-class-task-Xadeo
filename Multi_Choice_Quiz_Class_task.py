88a6d0c18811788267a23f096acd5dd341379962
Tong Chan <86693694+Xadeo@users.noreply.github.com>
2022-04-03 11:38:46 +1200

    Added to Multi_ChoiceGIU
    Added a font variable for prompt label and question label. Also added a loop to create radio buttons from the choices list and a output label in the GUI class.

--- Multi_Choice_Quiz_Class_task.py
+++ Multi_Choice_Quiz_Class_task.py
@@ -7,14 +7,23 @@ class Multi_ChoiceGUI:
         self.choice_var = StringVar()
         self.choice_var.set("")
         
-        self.font = ("Comic Sans MS", "10", "bold")
+        font = ("Comic Sans MS", "10", "bold")
 
-        prompt_Label = Label(parent, text = "Question.")
+        self.RBchoices_list = ["Vladivostok", "Astana", "Ulan Bator", "Lhasa"]
+
+        prompt_Label = Label(parent, text = "Question.", font = font)
         prompt_Label.grid(row = 0, column = 0)
 
-        quesiton_Label = Label(parent, text = "What is the capital of Mongolia?", font = self.font,  width = 40, height = 30)
+        quesiton_Label = Label(parent, text = "What is the capital of Mongolia?", font = font,  width = 30, height = 1)
         quesiton_Label.grid(row = 0, column = 1)
 
+        for choice in range(len(self.RBchoices_list)):
+            RBbtn = Radiobutton(parent, variable = self.choice_var, value = str(self.RBchoices_list[choice]), text = str(self.RBchoices_list[choice])
+                                        )
+            RBbtn.grid(row = 1 + choice, column = 1, sticky = N)
+
+        self.output_label = Label(parent, text = "Let's see if your knowlegde is impressive!", font = font)
+        self.output_label.grid(row = 5, column = 1, sticky = N)
 
 
 #main routine
