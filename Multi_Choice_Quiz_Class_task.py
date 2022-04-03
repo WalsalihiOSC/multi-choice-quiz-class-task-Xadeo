6ae904ddf5be926eb288963880adfa78315ea186
Tong Chan <86693694+Xadeo@users.noreply.github.com>
2022-04-03 12:22:33 +1200

    Added a display_answer function
    If-else statement.

--- Multi_Choice_Quiz_Class_task.py
+++ Multi_Choice_Quiz_Class_task.py
@@ -9,7 +9,7 @@ class Multi_ChoiceGUI:
         
         font = ("Comic Sans MS", "10", "bold")
 
-        self.RBchoices_list = ["Vladivostok", "Astana", "Ulan Bator", "Lhasa"]
+        RBchoices_list = ["Vladivostok", "Astana", "Ulaanbaatar", "Lhasa"]
 
         prompt_Label = Label(parent, text = "Question.", font = font)
         prompt_Label.grid(row = 0, column = 0)
@@ -17,15 +17,20 @@ class Multi_ChoiceGUI:
         quesiton_Label = Label(parent, text = "What is the capital of Mongolia?", font = font,  width = 30, height = 1)
         quesiton_Label.grid(row = 0, column = 1)
 
-        for choice in range(len(self.RBchoices_list)):
-            RBbtn = Radiobutton(parent, variable = self.choice_var, value = str(self.RBchoices_list[choice]), text = str(self.RBchoices_list[choice])
-                                        )
+        for choice in range(len(RBchoices_list)):
+            RBbtn = Radiobutton(parent, variable = self.choice_var, value = str(RBchoices_list[choice]), text = str(RBchoices_list[choice]),
+                                command = self.display_answer)
             RBbtn.grid(row = 1 + choice, column = 1, sticky = N)
 
         self.output_label = Label(parent, text = "Let's see if your knowlegde is impressive!", font = font)
         self.output_label.grid(row = 5, column = 1, sticky = N)
-
-
+    
+    def display_answer(self):
+        if str(self.choice_var.get()) == "Ulaanbaatar":
+            self.output_label.configure(text = str(self.choice_var.get()) + " is correct!!")
+        else:
+            self.output_label.configure(text = str(self.choice_var.get()) + " is not correct!!")
+        
 #main routine
 if __name__ == "__main__":        
     root = Tk()
