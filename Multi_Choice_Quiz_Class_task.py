b1023b152a44acc2fa3d05a96b8a80d65c22d28f
Tong Chan <86693694+Xadeo@users.noreply.github.com>
2022-04-06 14:09:16 +1200

    Created another class
    Improve code, and created data encapsulation.

--- Multi_Choice_Quiz_Class_task.py
+++ Multi_Choice_Quiz_Class_task.py
@@ -9,27 +9,37 @@ class Multi_ChoiceGUI:
         
         font = ("Comic Sans MS", "10", "bold")
 
-        RBchoices_list = ["Vladivostok", "Astana", "Ulaanbaatar", "Lhasa"]
-
+        #Setting up GUI
         prompt_Label = Label(parent, text = "Question.", font = font)
         prompt_Label.grid(row = 0, column = 0)
 
-        quesiton_Label = Label(parent, text = "What is the capital of Mongolia?", font = font,  width = 30, height = 1)
+        #Created an instance of the questions class
+        self.q = Questions()
+        quesiton_Label = Label(parent, text = self.q.questions, font = font,  width = 30, height = 1)
         quesiton_Label.grid(row = 0, column = 1)
-
-        for choice in range(len(RBchoices_list)):
-            RBbtn = Radiobutton(parent, variable = self.choice_var, value = str(RBchoices_list[choice]), text = str(RBchoices_list[choice]),
-                                command = self.display_answer)
+       
+        for choice in range(len(self.q.Multi_choice)):
+            RBbtn = Radiobutton(parent, variable = self.choice_var, value = str(self.q.Multi_choice[choice]),
+                                text = str(self.q.Multi_choice[choice]), command = self.display_answer)
             RBbtn.grid(row = 1 + choice, column = 1, sticky = N)
 
         self.output_label = Label(parent, text = "Let's see if your knowlegde is impressive!", font = font)
         self.output_label.grid(row = 5, column = 1, sticky = N)
     
     def display_answer(self):
-        if str(self.choice_var.get()) == "Ulaanbaatar":
-            self.output_label.configure(text = str(self.choice_var.get()) + " is correct!!")
+        self.output_label.configure(text = self.q.check_answer(self.choice_var.get()))
+
+class Questions:
+    def __init__(self):
+        self.questions = "What is the capital of Mongolia?"
+        self.answer = "Ulaanbaatar"
+        self.Multi_choice = ["Vladivostok", "Astana", "Ulaanbaatar", "Lhasa"]
+    
+    def check_answer(self, choice):
+        if str(choice) == self.answer:
+            return str(choice) + " is correct!!"
         else:
-            self.output_label.configure(text = str(self.choice_var.get()) + " is not correct!!")
+            return str(choice) + " is incorrect!!"
         
 #main routine
 if __name__ == "__main__":        
